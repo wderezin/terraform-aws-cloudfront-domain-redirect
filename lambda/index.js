756d1b70d9d1e15c28dca4d255fe8fac8801604d
Walter Derezinski <walter@daringway.com>
2020-07-11 20:34:48 -0400

    updated archive_file usage

--- lambda/index.js
+++ lambda/index.js
@@ -3,6 +3,17 @@ exports.handler = async (event) => {
 
   let host = request.headers.host[0].value
 
+  const body = `
+<\!DOCTYPE html>
+<html lang="en">
+<head><title>301 Moved Permanently</title></head>
+<body bgcolor="white">
+<center><h1>301 Moved Permanently</h1></center>
+<hr><center>CloudFront Lambda@Edge</center>
+</body>
+</html>
+`;
+
   if (host.split('.').length == 2) {
     return {
       status: '301',
@@ -12,7 +23,8 @@ exports.handler = async (event) => {
           key: 'Location',
           value: `https://www.${host}${request.uri}`
         }]
-      }
+      },
+      body: body
     };
   }
   return request;
